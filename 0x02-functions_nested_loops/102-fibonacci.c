8112c91891510cf2a46b63e9e96fb8b4a963f281
Oussama-hamdi <ohamdi776@gmail.com>
2023-02-22 07:42:14 -0800

    fibonacci sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int num1 = 1, num2 = 2, i, next;
+
+	printf("%ld, %ld, ", num1, num2);
+
+	for (i = 3; i <= 50; i++)
+	{
+		next = num1 + num2;
+
+		printf("%ld, ", next);
+
+		num1 = num2;
+		num2 = next;
+
+	}
+
+	printf("\n");
+	return (0);
+}
