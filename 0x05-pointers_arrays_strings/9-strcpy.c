f3d848e27c3ed6452b4a672a46084d4e633f177c
Oussama-hamdi <ohamdi776@gmail.com>
2023-02-28 08:23:30 -0800

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copy a string
+ * @dest: pointer
+ * @src: pointer
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
