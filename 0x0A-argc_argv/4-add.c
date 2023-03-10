9fd6a42beff2bc81e79d6d1f7d2a3a8962f814e1
Oussama-hamdi <ohamdi776@gmail.com>
2023-03-10 12:32:27 -0800

    add numbers from the arguments

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,9 +9,18 @@
 
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, sum = 0, count = 0, d = 0;
 
-	if (argc <= 2)
+	while (argv[d])
+	{
+		if (*argv[d] < 48 || *argv[d] > 57)
+		{
+			count++;
+		}
+		d++;
+	}
+
+	if (argc == count)
 	{
 		printf("%d\n", 0);
 		return (0);
