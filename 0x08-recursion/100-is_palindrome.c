43768fd6d471597fd7010d7631d15e5769c92ad4
root <ohamdi776@gmail.com>
2023-05-01 03:34:03 -0700

    is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,32 +1,54 @@
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
 
 /**
- * get_length - get the length of a string
- * @str: the string
- * Return: str length
+ * get_length - Get the length from a string
+ * @s: The string
+ * Return: Length of a string
  */
-
-int get_length(char *str)
+int get_length(char *s)
 {
-	int len = 0, i = 0;
-
-	while (str[i])
+	if (!*s)
 	{
-		len++;
-		i++;
+		return (0);
 	}
-	return len;
+	return (get_length(s + 1) + 1);
 }
+
+/**
+ * helper - Revesed string
+ * @s: Input string
+ * @length: String length
+ * Return: A reversed string
+ */
+int helper(char *s, int length)
+{
+	if (length <= 1)
+		return (1);
+	else if (*s == *(s + length - 1))
+	{
+		return (helper(s + 1, length - 2));
+	}
+	else
+		return (0);
+
+}
+
+/**
+ * is_palindrome - Checks if s is a palindrome
+ * @s: String to check
+ * Return: 1 if is string is a palindrome or 0 if not
+ */
 
 int is_palindrome(char *s)
 {
-	int length = get_length(s), i = 0;
+	int length;
 
-	while (i < length / 2)
-	{
-		if (s[i] != s[length - i - 1])
-			return (0);
-		i++;
-	}
-	return (1);
+	length = get_length(s);
+
+	if (length <= 1)
+		return (1);
+	return (helper(s, length));
 }
